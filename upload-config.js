480a2586350d39b26131ad6eb4977f26ece0f886
Steven Hubert <steven.hubert@gmail.com>
2020-12-15 19:02:25 -0700

    adding some mimetype verification to avoid anything getting uploaded

--- upload-config.js
+++ upload-config.js
@@ -1,14 +1,21 @@
-const multer = require('multer')
-const path = require('path')
+const multer = require("multer");
+const path = require("path");
 
 module.exports = {
-    storage : new multer.diskStorage({
-        destination : path.resolve(__dirname, ".","uploads"),
-        filename : function(req, file, callback) {
-            callback(null, file.originalname)
-        }
-        
-    }) 
-}
-
-
+  storage: new multer.diskStorage({
+    destination: path.resolve(__dirname, ".", "uploads"),
+    filename: function (req, file, callback) {
+      callback(null, file.originalname);
+    },
+  }),
+  limits: { fileSize: "10000000" },
+  fileFilter: (req, file, cb) => {
+    const fileTypes = /jpeg|jpg|png/;
+    const mimetype = fileTypes.test(file.mimetype);
+    const extname = fileTypes.test(path.extname(file.originalname));
+    if (mimetype && extname) {
+      return cb(null, true);
+    }
+    cb("Error: Only JPEG and PNG\'s accepted");
+  },
+};
